0debd65d4b19a25b3415074e0fe98dc581b6a168
ertrzyiks <mateusz.derks@gmail.com>
2019-06-05 21:28:37 +0200

    Add comments

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,7 @@
-self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting())
-})
-
-self.addEventListener('activate', () => {
-  return self.clients.claim()
-})
-
 self.addEventListener('push', event => {
-  var data = {};
+  let data = {}
   if (event.data) {
-    data = event.data.json();
+    data = event.data.json()
   }
 
   const notificationPromise = self.registration.showNotification(data.title, {
@@ -23,11 +15,28 @@ self.addEventListener('push', event => {
 self.addEventListener('notificationclick', function(event) {
   event.notification.close()
 
+  // `data` is passed in `showNotification` call above
   const { redirect_url } = event.notification.data
 
   if (!redirect_url) return
 
   event.waitUntil(
     clients.openWindow(redirect_url)
-  );
+  )
+})
+
+
+/**
+ * We don't mess with network so can be adventurous and activate service worker right away
+ * with `skipWaiting` and `claim` calls.
+ *
+ * In more complex setup we may need to patiently wait for update or offer a page reload:
+ * https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+ */
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting())
+})
+
+self.addEventListener('activate', () => {
+  return self.clients.claim()
 })
